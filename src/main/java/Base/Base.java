203436e504171502669891b8f78780eece14c39c
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-03-16 11:20:50 +0300

    fix for the chrome options and before method issue

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -26,7 +26,7 @@ public class Base {
 
             driver.manage().deleteAllCookies();
             driver.manage().window().maximize();
-            System.out.println("hello spc");
+            System.out.println("hello spc jenkins");
         }
 
         public void aftermethod() {
